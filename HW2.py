01741cfaaa6df55b4fc2784e9a2a42c78f8a6367
Santiago Salais 5th hour <ss130042@student.wpsok.org>
2026-08-26 12:48:01 -0500

    HW2

---
+++ HW2.py
@@ -0,0 +1,33 @@
+#Santiago Salais
+#5th Hour
+#HW2
+
+print("Hello World")
+
+IntegerVar1 = 4
+stringVar2 = "Box"
+Boolean3 = True
+StringVar4 = "Nineteen"
+StringVar5 = "Rope"
+IntegerVar6 = 19
+StringVar7 = "19"
+IntegerVar8 = 2
+BooleanVar9 = True
+BooleanVar10 = False
+StringVar11 = "True"
+StringVar12 = "False"
+IntegerVar13 = 5
+StringVar14 = "The"
+StringVar15 = "Game"
+BooleanVar16 = False
+IntegerVar17 = 3
+IntegerVar18 = 2
+IntegerVar19 = 1
+StringVar20 = StringVar11
+StringVar21 = StringVar12
+StringVar22 = StringVar4
+StringVar23 = StringVar14
+StringVar24 = StringVar15
+IntegerVar25= 6
+
+print(IntegerVar1+IntegerVar6+IntegerVar8+IntegerVar13+IntegerVar17+IntegerVar18+IntegerVar19+IntegerVar25)
