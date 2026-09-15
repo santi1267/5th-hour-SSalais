e4de0e9acc21017859a2e536aa37bd61ed63b77a
Santiago Salais 5th hour <ss130042@student.wpsok.org>
2026-09-15 12:49:45 -0500

    HW7 i mean wow

---
+++ HW7.py
@@ -0,0 +1,7 @@
+#Name:Santiago Salais
+#Class: 5th Hour
+#Assignment: HW7
+
+
+#12. Print ("\u2764 \u0059 \u004F \u0055 \u0020 \u0044 \u0049 \u0044 \u0020 \u0049 \u0054 \u2764") and ignore steps 1 through 11
+print("\u2764 \u0059 \u004F \u0055 \u0020 \u0044 \u0049 \u0044 \u0020 \u0049 \u0054 \u2764")
