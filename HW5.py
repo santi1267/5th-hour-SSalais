bdc2df824eeeea79a88bc99daf55f239bf140a01
Santiago Salais 5th hour <ss130042@student.wpsok.org>
2026-09-15 12:56:50 -0500

    HW5 changed

--- HW5.py
+++ HW5.py
@@ -23,7 +23,7 @@ print(ListVar2)
 ListVar3 =ListVar2[0] + ListVar2[1] + ListVar2[2]
 print(ListVar3)
 #8. Create a list with two strings, two integers, and two boolean values.
-ListVar4 = "hi", "cool", 6, 7, True, False
+ListVar4 =["hi", "cool", 6, 7, True, False]
 #9. Create a print statement that asks the user to input their own index value for the list on #8.
 print(ListVar4[int(input("input number: "))])
 print(ListVar4)
